c7e564bf82f52f9ec30f4f9b6ad6e07a641c5384
NazRudy <nazrudy2014@gmail.com>
2022-03-03 16:08:59 +0200

    add MainActivity3

---
+++ app/src/main/java/com/naz/lesson1homework01/MainActivity3.java
@@ -0,0 +1,14 @@
+package com.naz.lesson1homework01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
